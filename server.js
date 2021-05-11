7efffab4d4ad3da708a888e7d317a49e34e27de5
Javier Pons <jpescandell@hotmail.com>
2021-05-11 14:35:37 +0300

    server

--- server.js
+++ server.js
@@ -2,3 +2,18 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt");
 const crypto = require("crypto");
+
+
+let app = express();
+
+app.use(bodyParser.json());
+
+
+// DB
+
+let registeredUsers = [];
+let loggedSessions = [];
+
+
+app.listen(3009)
+console.log("Running in port 3009")
